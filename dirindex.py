3532aa22dcc84ca8525c4bb73a349c3187855a15
Muflone <webreg@vbsimple.net>
2012-09-11 13:02:59 +0200

    Addes filters for symlinks and hidden files and directories

--- dirindex.py
+++ dirindex.py
@@ -28,13 +28,16 @@ class Template(object):
 
 class ScannerOptions(object):
   def __init__(self, divider, directories_first, 
-    include_directories, include_files,
+    include_directories, include_files, 
+    include_symlinks_directories, include_symlinks_files,
     include_hidden_directories, include_hidden_files,
     recursive, max_depth):
     self.divider = divider
     self.directories_first = directories_first
     self.include_directories = include_directories
     self.include_files = include_files
+    self.include_symlinks_directories = include_symlinks_directories
+    self.include_symlinks_files = include_symlinks_files
     self.include_hidden_directories = include_hidden_directories
     self.include_hidden_files = include_hidden_files
     self.max_depth = max_depth
@@ -51,24 +54,25 @@ class Scanner(object):
   
   def scan(self, path, template):
     self.template = Template(template)
+    self.root = path
     self._scan_directory(path, 1)
 
   def _scan_directory(self, path, depth):
     listItems = os.listdir(path)  
+    listDirs = [item for item in listItems if os.path.isdir(os.path.join(path, item))]
+    listFiles = [item for item in listItems if not os.path.isdir(os.path.join(path, item))]
+    listItems = []
     if self.options.directories_first:
       # Sort directories first
-      listDirs = [item for item in listItems if os.path.isdir(os.path.join(path, item))]
       listDirs.sort()
-      listFiles = [item for item in listItems if not os.path.isdir(os.path.join(path, item))]
       listFiles.sort()
-      listItems = []
-      if self.options.include_directories:
-        # Include directories
-        listItems = listDirs
-      if self.options.include_files:
-        # Include files
-        listItems.extend(listFiles)
-    else:
+    # Filter for directories
+    if self.options.include_directories:
+      listItems = listDirs
+    # Filter for files
+    if self.options.include_files:
+      listItems.extend(listFiles)
+    if not self.options.directories_first:
       # Sort files and directories together
       listItems.sort()
   
@@ -77,12 +81,26 @@ class Scanner(object):
     if 1==1:
       for fileName in listItems:
         # Scan every item inside the directory
+        bInclude = True
         dictDetails = self._get_file_details(path, fileName, depth)
-        if dictDetails['FORD'] == 'd':
-          # Delay subfolders scan
-          listDirs.append(dictDetails['PATH'])
-        
-        self.template.write(**dictDetails)
+        bIsDirectory = dictDetails['DIRECTORY'] == 'y'
+        # Skip symlinks if not requested
+        if dictDetails['LINK'] == 'y':
+          if bIsDirectory and not self.options.include_symlinks_directories:
+            bInclude = False
+          if not bIsDirectory and not self.options.include_symlinks_files:
+            bInclude = False
+        # Skip hidden files/directories if not requested
+        if dictDetails['HIDDEN'] == 'y':
+          if bIsDirectory and not self.options.include_hidden_directories:
+            bInclude = False
+          if not bIsDirectory and not self.options.include_hidden_files:
+            bInclude = False
+        if bInclude:
+          if bIsDirectory:
+            # Delay subfolders scan
+            listDirs.append(dictDetails['PATH'])
+          self.template.write(**dictDetails)
       if self.options.recursive and \
         (self.options.max_depth == 0 or depth < self.options.max_depth):
         depth += 1
@@ -99,10 +117,13 @@ class Scanner(object):
     dictDetails = {}
     dictDetails['FILENAME'] = fileName
     dictDetails['PARENT'] = path
-    dictDetails['PATH'] = sFilePath
+    dictDetails['PATH'] = sFilePath[len(self.root) + 1:]
+    dictDetails['FULLPATH'] = sFilePath
     dictDetails['DEPTH'] = depth
-    dictDetails['FORD'] = os.path.isdir(sFilePath) and 'd' or 'f'
-    lSize = dictDetails['FORD'] == 'f' and os.path.getsize(sFilePath) or 0
+    dictDetails['DIRECTORY'] = os.path.isdir(sFilePath) and 'y' or 'n'
+    dictDetails['LINK'] = os.path.islink(sFilePath) and 'y' or 'n'
+    dictDetails['HIDDEN'] = fileName[0] == '.' and 'y' or 'n'
+    lSize = dictDetails['DIRECTORY'] == 'n' and os.path.getsize(sFilePath) or 0
     if self.template.size:
       dictDetails['SIZE'] = lSize < self.options.divider and \
         '%d B' % lSize or \
@@ -139,10 +160,14 @@ class Scanner(object):
         fileName.rsplit('.', 1)[1] or ''
     if self.template.ext:
       dictDetails['EXT'] = '.' in fileName and fileName.count('.') and \
-        '.%s' % fileName.rsplit('.',1 )[1] or ''
+        fileName.rsplit('.', 1)[1] or ''
     return dictDetails
 
 if __name__=='__main__':
-  options = ScannerOptions(1024, True, True, True, True, True, True, 2)
+  options = ScannerOptions(divider=1024, directories_first=True,
+    include_directories=True, include_files=True,
+    include_symlinks_directories=True, include_symlinks_files=True,
+    include_hidden_directories=False, include_hidden_files=False,
+    recursive=True, max_depth=1)
   scanner = Scanner(options)
-  scanner.scan(os.path.expanduser('~'), "{FORD} {PATH} {EXT} {TYPE} {MIME}")
+  scanner.scan(os.path.expanduser('~'), "{DIRECTORY} {LINK} {PATH} .{EXT}")
