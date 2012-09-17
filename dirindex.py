64a3d139b8b1d81bd350331eeb261e0c9b590c19
Muflone <webreg@vbsimple.net>
2012-09-17 14:21:09 +0200

    Added INDEX and COUNT placeholder fields

--- dirindex.py
+++ dirindex.py
@@ -216,7 +216,8 @@ class Scanner(object):
       'PATH': os.path.relpath(dirpath, self.root_dir),
       'FULLPATH': dirpath,
       'DEPTH': depth,
-      'COUNT': count
+      'COUNT': count,
+      'INDEX': 0
     }
 
   def _get_file_details(self, path, fileName, depth):
@@ -230,6 +231,10 @@ class Scanner(object):
     dictDetails['PATH'] = os.path.relpath(sFilePath, self.root_dir)
     dictDetails['FULLPATH'] = sFilePath
     dictDetails['DEPTH'] = depth
+    # INDEX and COUNT placeholders, they will be set later after the scan
+    # to properly set them if the file has to be skipped
+    dictDetails['INDEX'] = 0
+    dictDetails['COUNT'] = 0
     # Obtain file attributes
     is_directory = os.path.isdir(sFilePath)
     dictDetails['DIRECTORY'] = is_directory and 'y' or 'n'
