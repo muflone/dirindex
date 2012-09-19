81c14db53354451a96f60b90e761e18c22e244e5
Muflone <webreg@vbsimple.net>
2012-09-19 18:57:34 +0200

    Added copyright notices

--- dirindex.py
+++ dirindex.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+##
+#       Project : dirindex
+#   Description : A tool to create an HTML index starting from a directory.
+#        Author : Muflone <muflone@vbsimple.net>
+#     Copyright : 2012 Fabio Castelli
+#       License : GPL-2+
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the Free
+#  Software Foundation; either version 2 of the License, or (at your option)
+#  any later version.
+# 
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+##
 
 import os
 import os.path
